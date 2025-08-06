c0eb65168790b8a86df5f6072c2ac14bc91ef9d9
Bruno Desktop <carecsnay1@gmail.com>
2025-08-06 19:39:41 -0300

    feat: add protocol to remotePatterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
         hostname: "fsc-projects-static.s3.us-east-1.amazonaws.com",
       },
     ],
