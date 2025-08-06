309c59497270bffb1abca6f9e0a0dc212b7225a7
Bruno Nitro5 <carecsnay1@gmail.com>
2025-08-06 15:25:32 -0300

    feat: add Header component with authentication and menu functionality | refactor: adjust component root in common folder.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
-import Header from "./components/header";
+import Image from "next/image";
 
-export default function Home() {
-  return <Header />;
+import { db } from "@/db";
+
+import Header from "./components/common/header";
+
+export default async function Home() {
+  const products = await db.query.productTable.findMany({});
+  return (
+    <>
+      <Header />;
+      <div className="space-y-6 p-5">
+        <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full rounded-md"
+        />
+
+        <Image
+          src="/banner-02.png"
+          alt="Seja autêntico"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full rounded-md"
+        />
+      </div>
+    </>
+  );
 }
